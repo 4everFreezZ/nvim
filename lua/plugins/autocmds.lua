4660b02036f5a7bd1b2ab3e70aeca5b0e93e7299
4everFreezZ <2195437639@qq.com>
2025-09-02 19:30:39 +0800

    add autocmd.lua

--- lua/plugins/autocmds.lua
+++ lua/plugins/autocmds.lua
@@ -1,12 +1,19 @@
--- ~/.config/nvim/lua/config/autocmds.lua
-local augroup = vim.api.nvim_create_augroup("user_cmds", { clear = true })
+-- ~/.config/nvim/lua/plugins/disable-spellcheck.lua
+return {
+  -- A name for our custom rule, this can be anything unique
+  "my-custom-rules/disable-spellcheck",
+  -- Use the "FileType" event to trigger this setup.
+  -- This means it will run after a file is opened and its type is detected.
+  event = "FileType",
 
--- 在打开特定文件类型时，强制关闭拼写检查
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup,
-  pattern = { "markdown", "gitcommit", "text" }, -- 这里可以按需添加其他文件类型
-  callback = function()
-    vim.opt_local.spell = false
+  -- The config function is where we define the autocommand
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "markdown", "gitcommit", "text" },
+      callback = function()
+        vim.opt_local.spell = false
+      end,
+      desc = "Disable spell check for specific file types.",
+    })
   end,
-  desc = "Disable spell check for specific file types.",
-})
+}
