e783ca2cfe74540edb08c404139aa44bf1d40743
4everFreezZ <2195437639@qq.com>
2025-09-03 13:36:17 +0800

    add leetcode.lua

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,12 +1,24 @@
-	return {
-    "kawre/leetcode.nvim",
-    build = ":TSUpdate html", -- 若有nvim-treesitter则执行
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "nvim-treesitter/nvim-treesitter",
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html", -- 若有nvim-treesitter则执行
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+
+    -- optional
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    -- 个人化配置区域
+    lang = "java",
+    cn = { -- leetcode.cn
+      enabled = true, ---@type boolean
+      translator = true, ---@type boolean
+      translate_problems = true, ---@type boolean
     },
-    opts = {
-        -- 个人化配置区域
-    },
+    ---@type boolean
+    logging = true,
+  },
 }
